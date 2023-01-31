40c0f689dff0a4a0df8fe3a6f2596b88f99768c3
GodoiTheCreator <eduardogodoisilveira@gmail.com>
2023-01-30 21:10:33 -0300

    Adding first day's exercises

---
+++ exercises/1_day/dataypes.js
@@ -0,0 +1,4 @@
+console.log("'stringType's type is", typeof stringType);
+console.log("'booleanType's type is", typeof booleanType);
+console.log("'undefindedType's type is", typeof undefinedType);
+console.log("'nullType's type is", typeof nullType);
---
+++ exercises/1_day/variable.js
@@ -0,0 +1,27 @@
+let stringType = "This is a string type";
+let booleanType = true;
+let undefinedType = undefined;
+let nullType = null;
+
+let withoutValueOne;
+let withoutValueTwo;
+let withoutValueThree;
+let withoutValueFour;
+
+let firstName = "Eduardo";
+let lastName = "Silveira";
+let maritalStatus = "Dating.";
+let country = "Brazil";
+let age = 20;
+
+let firstNameTwo = "Eduardo",
+  lastNameTwo = "Silveira",
+  maritalStatusTwo = "Dating.",
+  countryTwo = "Brazil",
+  ageTwo = 20;
+
+let myAge = 25;
+let yourAge = 30;
+
+console.log("I am", myAge, "years old.");
+console.log("You are", yourAge, "years old.");
