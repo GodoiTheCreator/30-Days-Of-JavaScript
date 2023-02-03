2c87d00ee0f4582d63a98668f51a8ae9e4332a7e
GodoiTheCreator <eduardogodoisilveira@gmail.com>
2023-02-03 15:28:48 -0300

    Adding exercises day 2

---
+++ exercises/2_day/exerciseTwo.js
@@ -0,0 +1,15 @@
+let statement =
+  "The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.";
+
+console.log(statement);
+
+let quote =
+  "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.";
+
+console.log(quote);
+
+console.log(typeof Number("10") == typeof 10);
+console.log(parseFloat("9.8") == 10);
+console.log("pargon".includes("on") && "python".includes("on"));
+console.log(Math.floor(Math.random() * 101));
+console.log("JavaScript".charAt(Math.floor(Math.random() * 11)));
